ed306ddf1dad81fd6d921e4635be84733c820018
shubham chapagain <suvam.chapagain456@gmail.com>
2024-05-15 01:00:16 +0545

    Added linkedlist1

---
+++ linkdlist1.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+    
+    
+        
+
+    def find_middle_node(self):
+        temp=self.head
+        temp2=self.head
+        while temp2.next is not None:
+            temp=temp.next
+            temp2=temp2.next.next
+        return temp
+        
+        
+        
+
+
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value )
